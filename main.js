23f5350080608546c014a9eeedff1db9d7b414f9
alittlehotcurry <45229969+alittlehotcurry@users.noreply.github.com>
2019-01-10 15:07:22 +0530

    Edit line item

--- main.js
+++ main.js
@@ -5,15 +5,17 @@ var ul = document.getElementById("ul");
 addBtn.onclick = function () {
   if (listDetail.value !== "") {
     var li = document.createElement("LI");
-    var liText = document.createTextNode(listDetail.value);
-    var liClose = document.createTextNode("-");
-    liClose.className = "close";
-    li.appendChild(liText);
     ul.appendChild(li);
   }
   else {
     alert("List item cannot be empty");
   }
+
+  var entry = document.createElement("SPAN");
+  var entryText = document.createTextNode(listDetail.value);
+  entry.className = "userEntry";
+  entry.appendChild(entryText);
+  li.appendChild(entry);
 
   var span = document.createElement("SPAN");
   var spanText = document.createTextNode("\u00D7");
@@ -27,6 +29,18 @@ addBtn.onclick = function () {
       this.parentElement.style.display = "none";
     }
   }
+
+  var edit = document.createElement("SPAN");
+  var eText = document.createTextNode("\u270E");
+  edit.className = "edit";
+  edit.appendChild(eText);
+  li.appendChild(edit);
+  edit.onclick = function () {
+    var p = prompt("Edit your entry");
+    var entry = this.parentElement.getElementsByClassName("userEntry")[0]; 
+    entry.innerText = p;
+  }
+
 
   var liTag = document.getElementsByTagName("LI");
 
